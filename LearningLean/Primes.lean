0ad8fbeea49fff2dec88aef90ad03f18e2744089
Gregor <gregor.mitscha-baude@gmx.at>
2024-07-30 18:08:54 +0200

    use local implicit variables, let's see if this is a good idea

--- LearningLean/Primes.lean
+++ LearningLean/Primes.lean
@@ -4,11 +4,12 @@
 
 def prime (p : Nat) :=
   (p ≠ 0) ∧ (p ≠ 1) ∧
-  ∀ x : Nat, x ∣ p → x = 1 ∨ x = p
+  ∀ x : Nat, (x ∣ p) → (x = 1) ∨ (x = p)
 
+-- TODO: show that this is equivalent to prime
 def prime' (p : Nat) :=
   (p ≠ 0) ∧ (p ≠ 1) ∧
-  ∀ a b : Nat, p ∣ a*b → p ∣ a ∨ p ∣ b
+  ∀ a b : Nat, (p ∣ a*b) → (p ∣ a) ∨ (p ∣ b)
 
 structure Prime where
   val : Nat
@@ -18,17 +19,28 @@ structure Field (p : Prime) where
   val : Nat
   less : val < p.val
 
--- taking a number mod p produces something < p
-theorem mod_smaller (x : Nat) (p : Prime) : x % p.val < p.val :=
- have gt_zero : p.val > 0 :=
-    (p.prime.left : p.val ≠ 0)
-    |> Nat.ne_zero_iff_zero_lt.mp
- show x % p.val < p.val from Nat.mod_lt x gt_zero
+namespace Field
+  variable {p : Prime} (x : Field p)
 
--- create field element from a Nat
-def Field.from { p: Prime } (x : Nat) : Field p :=
-  Field.mk (x % p.val) (mod_smaller x p)
+  -- taking a number mod p produces something < p
+  theorem mod_smaller (x : Nat) : x % p.val < p.val :=
+    have gt_zero : p.val > 0 :=
+        (p.prime.left : p.val ≠ 0)
+        |> Nat.ne_zero_iff_zero_lt.mp
+    show x % p.val < p.val from Nat.mod_lt x gt_zero
 
--- addition
-instance { p : Prime } : Add (Field p) where
-  add x y := x.val + y.val |> Field.from
+  -- create field element from a Nat
+  def create (x : Nat) : Field p :=
+    mk (x % p.val) (mod_smaller x)
+
+  -- addition
+  instance : Add (Field p) where
+    add x y := x.val + y.val |> create
+
+  -- addition: neutral element
+
+  def zero : Field p := create 0
+
+  theorem neutral : x + zero = x := sorry
+
+end Field
