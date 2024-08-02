13733d7c1035dbbd388fc18ae1e47f080f020da2
Gregor <gregor.mitscha-baude@gmx.at>
2024-08-02 16:39:16 +0200

    prove k reduction but with denominators multiplied out

--- LearningLean/Binomial.lean
+++ LearningLean/Binomial.lean
@@ -17,6 +17,8 @@ variable (n k : Nat)
 
 theorem zero : Bin n 0 = 1 := by cases n <;> rfl
 
+theorem recursive : Bin (n + 1) (k + 1) = Bin n (k + 1) + Bin n k := rfl
+
 
 theorem greater : ∀ n k, n < k → Bin n k = 0 := fun n => by
   induction n with
@@ -52,16 +54,84 @@ theorem greater1 : Bin n (n+1) = 0 := by
 theorem same : Bin n n = 1 := by
   induction n with
   | zero => rfl -- `Bin 0 0 = 1`
-  | succ n same_n =>
-    -- goal: `Bin (n + 1) (n + 1) = 1`
-    -- expands to `Bin n (n + 1) + Bin n n = 0 + 1` by `greater1` and IH
-    simp [Bin, greater1 n, same_n]
+  | succ n same_n => simp [recursive n n, greater1 n, same_n]
 
 
-theorem k_reduction : ∀ n k, Bin n (k+1) = n/(k+1) * Bin n k := fun n => by
-  sorry
+theorem one : Bin n 1 = n := by
+  induction n with
+  | zero => rfl -- `Bin 0 1 = 0`
+  | succ n one_n => simp [recursive n 0, one_n, zero]
 
-theorem n_reduction (n k : Nat) : Bin (n+1) k = (n+1)/(n-k+1) * Bin n k := by
+
+theorem smaller1 : Bin (n + 1) n = n + 1 := by
+  induction n with
+  | zero => rfl -- `Bin 1 0 = 1`
+  | succ n smaller1_n =>
+    simp [recursive (n + 1), same (n + 1), smaller1_n]
+    rw [Nat.add_comm]
+
+
+theorem k_reduction : ∀ n k, n > k →
+
+  (k + 1) * (Bin n (k + 1)) = (n - k) * (Bin n k)
+
+  := fun n => by
+  induction n with
+  | zero =>
+    intro _ (_ : 0 > _) -- impossible
+    simp; contradiction
+
+  | succ n k_reduction_n =>
+    intro k
+    cases k with
+    -- the case k = 0 is easy, handle it separately to be able to expand the recursive definition below
+    | zero => simp [one (n + 1), zero]
+
+    -- case for k + 1
+    | succ k =>
+      intro (hk : n + 1 > k + 1)
+
+      -- we have n > k, so either n = k + 1 or n > k + 1;
+      -- when n = k + 1, we can't neatly use the induction hypothesis, so we handle it separately (it easily follows from `smaller1`)
+      cases (Nat.le_of_lt_succ hk |> Nat.eq_or_lt_of_le : k + 1 = n ∨ k + 1 < n) with
+
+      -- case n = k + 1
+      | inl k1_eq_n =>
+        have n_eq_k1 : n = k + 1 := by apply Eq.symm; assumption
+        simp [n_eq_k1, same (k + 2), smaller1 (k + 1), Nat.add_sub_cancel_left]
+
+      -- main case: n > k + 1
+      | inr n_gt_k1 =>
+        have n_gt_k : n > k := by apply Nat.lt_of_succ_lt; assumption
+
+        -- goal: `(k + 2) * Bin (n + 1) (k + 2) = (n - k) * Bin (n + 1) (k + 1)`
+
+        simp [Bin]
+        -- expanding both sides gave us:
+        -- `(k + 2) * (Bin n (k + 2) + Bin n (k + 1)) = (n - k) * (Bin n (k + 1) + Bin n k)`
+
+        repeat rw [Nat.mul_add] -- expand the multiplication on both sides
+
+        -- we can apply the induction hypothesis twice to make all 4 terms multiples of `Bin n (k + 1)`!
+        have n_k2 : (k+2) * Bin n (k+2) = (n - (k + 1)) * Bin n (k+1) := k_reduction_n (k + 1) n_gt_k1
+        have n_k1 : (k+1) * Bin n (k+1) = (n - k) * Bin n k := k_reduction_n k n_gt_k
+
+        simp [n_k2, ← n_k1]
+
+        repeat rw [← Nat.add_mul _ _ (Bin n (k+1))]; -- collect multiples on both sides
+
+        have factors_eq : ∀ k, n > k → n - k + (k + 1) = n + 1 := fun k n_gt_k => by
+          rw [← Nat.sub_add_comm (Nat.le_of_lt n_gt_k), Nat.add_comm k, ← Nat.add_assoc, Nat.add_sub_cancel]
+
+        rw [factors_eq k n_gt_k, factors_eq (k + 1) n_gt_k1]
+
+-- helper lemma (does this not exist??)
+theorem n_div_n : ∀ n, n > 0 -> n/n = 1 := fun n n_gt_0 => by
+  have : 1 * n/n = 1 := Nat.mul_div_cancel 1 n_gt_0
+  simp_all
+
+
+theorem n_reduction : ∀ n k, Bin (n + 1) k = (n + 1)/(n - k + 1) * Bin n k := by
   sorry
 
 end Bin
